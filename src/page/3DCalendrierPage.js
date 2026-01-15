180693a7c5925e894bd7d604c74453adcab57e2d
qwex03 <qlecordier@gmail.com>
2026-01-15 14:44:00 +0100

    ajout de point sur le globe et des fenetres modales au clique

--- src/page/3DCalendrierPage.js
+++ src/page/3DCalendrierPage.js
@@ -1,16 +1,18 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Globe from "react-globe.gl";
 import * as THREE from "three";
+import gpData from "../json/calendar.json"; 
+
 
 function CalendrierPage() {
   const globeRef = useRef();
+  const [selectedGP, setSelectedGP] = useState(null);
 
   useEffect(() => {
     if (!globeRef.current) return;
 
     const scene = globeRef.current.scene();
     const starCount = 100000;
-
     const starGeometry = new THREE.BufferGeometry();
     const starVertices = [];
 
@@ -51,7 +53,6 @@ function CalendrierPage() {
 
     const stars = new THREE.Points(starGeometry, starMaterial);
     scene.add(stars);
-
   }, []);
 
   return (
@@ -62,7 +63,45 @@ function CalendrierPage() {
         bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
         backgroundColor="#000000"
         showAtmosphere={true}
+        pointsData={gpData}
+        pointLat="lat"
+        pointLng="lng"
+        pointColor={() => "red"} 
+        pointAltitude={0.02} 
+        pointRadius={0.8} 
+        onPointClick={(point) => setSelectedGP(point)}
       />
+
+      {selectedGP && (
+       <div
+        className="
+        absolute top-1/2 left-1/2 
+        transform -translate-x-1/2 -translate-y-1/2
+        bg-white/10 backdrop-blur-md 
+        border border-white/30
+        rounded-2xl shadow-2xl
+        p-6 w-80 max-w-[90%]
+        text-white
+        animate-fadeIn
+        "
+      >
+        <h2 className="text-2xl font-extrabold mb-2">{selectedGP.gp}</h2>
+        <p className="text-sm mb-1"><span className="font-semibold">Circuit:</span> {selectedGP.circuit}</p>
+        <p className="text-sm mb-1"><span className="font-semibold">Ville:</span> {selectedGP.city}</p>
+        <p className="text-sm mb-4"><span className="font-semibold">Date:</span> {selectedGP.date}</p>
+        <button
+        onClick={() => setSelectedGP(null)}
+        className="
+        w-full py-2 bg-red-600 hover:bg-red-500 
+        rounded-xl font-semibold
+        transition-colors duration-200
+        shadow-md
+        "
+        >
+        Fermer
+        </button>
+      </div>
+      )}
     </div>
   );
 }
