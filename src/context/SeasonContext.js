50c67e5fc44d7186498325da0772d9b161884beb
qwex03 <qlecordier@gmail.com>
2026-02-16 16:13:52 +0100

    changement de l'année en cours par défaut

--- src/context/SeasonContext.js
+++ src/context/SeasonContext.js
@@ -3,7 +3,7 @@ import { createContext, useContext, useState } from "react";
 const SeasonContext = createContext();
 
 export function SeasonProvider({ children }) {
-  const [season, setSeason] = useState(2025);
+  const [season, setSeason] = useState(2026);
 
   return (
     <SeasonContext.Provider value={{ season, setSeason }}>
